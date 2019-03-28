bca6a70ac6a3e2096064d5d38a27c84b86da1519
thunderpeal <pealsgun@gmail.com>
2019-03-28 12:18:55 +0300

    Отделил поле 10x10 от декорирующих конструкций

--- Kursovaya/Kursovaya.cpp
+++ Kursovaya/Kursovaya.cpp
@@ -3,59 +3,40 @@
 #include "Sea_Battle.h"
 
 int ** set_0(int **array) {
-	for (int i = 0; i < 12; i++) {
-		array[i] = new int[12];
-		for (int j = 0; j < 12; j++) {
-			if (i == 0 && j > 1 ) {
-				array[i][j] = 9; //буквы
-			}
-			else if ((i <= 1 && j == 0) || i == 1 || j == 1) {
-				array[i][j] = 8; //пробелы
-			}
-			else if (j == 0 && i != 0) {
-				array[i][j] = 7; //цифры
-			}
-			else {
-				array[i][j] = 0; //собственно, поля
-			}
-			
-		}
+	for (int i = 0; i < 10; i++) {
+		array[i] = new int[10];
+		for (int j = 0; j < 10; j++) { array[i][j] = 0; } //собственно, поля
 	}
 	return array;
 }
 
 void simple_vivod (int **array, const char alphabet[10], int i) {
-	int checker_1_1 = 0; //для корректного отображения букв над полем
-	int checker_1_2 = i-2; //а это для цифр, у нас они с 3 строки идут
 	for (int j = 0; j < 12; j++) {
-		switch (array[i][j]) {
-		case 0:
-			cout << ". ";
-			break;
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-			cout << array[i][j] << " ";
-			break;
-		case 7:
-			checker_1_2 += 1;
-			if (checker_1_2 == 10) { cout << checker_1_2; }
-			else { cout << checker_1_2 << " "; }
-			break;
-		case 8:
-			cout << "  ";
-			break;
-		case 9:
-			cout << alphabet[checker_1_1] << " ";
-			checker_1_1 += 1;
-			break;
-		case -1:
-			cout << "o ";
-			break;
-		case (-2):
-			cout << "x ";
-			break;
+		if (i == 0 && j >= 2) { cout << alphabet[j - 2] << " ";  }
+		else if (i == 1) { break; }
+		else if ((i > 1 && j == 1) || (i == 0 && j < 2)) { cout << "  "; }
+		else if ( i > 1 && j == 0) { 
+			if (i-1 != 10) {cout << i - 1 << " ";}
+			else { cout << i - 1; }	
+		}
+		else {
+			switch (array[i-2][j-2]) {
+			case 0:
+				cout << ". ";
+				break;
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				cout << array[i-2][j-2] << " ";
+				break;
+			case -1:
+				cout << "o ";
+				break;
+			case (-2):
+				cout << "x ";
+				break;
+			}
 		}
 	}
 }
@@ -72,6 +53,11 @@ void vivod(int **array_1, int **array_2, const char alphabet[10]) {
 	cout << endl << endl << endl; //отступ снизу
 }
 
+//int ** ras(int **array) {
+
+//}
+
+
 int main()
 {
 	const char alphabet[10] = { 'A','B','C','D','E','F','G','H','I','J' };
@@ -81,6 +67,4 @@ int main()
 	enemy_zones = set_0(enemy_zones);
 	vivod(self_zones, enemy_zones, alphabet);
 }
-
 //ДЕЛАЕМ РАССТАНОВКУУУ
-
